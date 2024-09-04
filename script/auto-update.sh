44290eec1f7b2422246c146d7a5b90954e82082a
sapics <gv.nishino@gmail.com>
2024-09-04 21:59:27 +0900

    Fix auto update

--- script/auto-update.sh
+++ script/auto-update.sh
@@ -1,5 +1,5 @@
 # get current version from package.json
-RES1=`find data -name '*.dat' -size -1000k 2>/dev/null`
+RES1=`find data -name '*.dat' -size -100k 2>/dev/null`
 if [ -z "$RES1" ]; then
 	if ! git diff --quiet HEAD -- *.dat; then
 		VERT=`node -p "var j=require('./package.json');j.version=j.version.split('.').slice(0,-1).join('.')+'.'+require('dayjs')().format('YYYYMMDD');require('fs').writeFileSync('./package.json',JSON.stringify(j,null,2));j.version;"`
