9db2da3925c474f82aa9c0b28fa07d169637e3fc
sapics <gv.nishino@gmail.com>
2024-09-08 20:40:54 +0900

    Fix for no write permission

--- script/updatedb-first.mjs
+++ script/updatedb-first.mjs
@@ -8,15 +8,18 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 var dataDir = path.join(__dirname, '../data')
+var hasDataDir = true
 try{
 	if(!fs.existsSync(dataDir)) {
 		fs.mkdirSync(dataDir)
 	}
 }catch(e){
+	// No write access permission
+	hasDataDir = false
 	console.log(e)
 }
 
 var dataFile = path.join(__dirname, '../data/4-1.dat')
-if(!fs.existsSync(dataFile)) {
+if(hasDataDir && !fs.existsSync(dataFile)) {
 	update()
 }
