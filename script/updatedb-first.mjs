29faa69e12e7e4525ffa07f6b2164c821967705e
sapics <gv.nishino@gmail.com>
2024-09-08 20:31:40 +0900

    make data dir at first

--- script/updatedb-first.mjs
+++ script/updatedb-first.mjs
@@ -7,6 +7,15 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+var dataDir = path.join(__dirname, '../data')
+try{
+	if(!fs.existsSync(dataDir)) {
+		fs.mkdirSync(dataDir)
+	}
+}catch(e){
+	console.log(e)
+}
+
 var dataFile = path.join(__dirname, '../data/4-1.dat')
 if(!fs.existsSync(dataFile)) {
 	update()
