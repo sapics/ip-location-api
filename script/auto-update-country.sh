2f485ff0264c62b0b7c3668e6bbf05445d44018d
sapics <gv.nishino@gmail.com>
2024-09-16 11:50:21 +0900

    Fix auto-update-country for browser

--- script/auto-update-country.sh
+++ script/auto-update-country.sh
@@ -1,27 +1,20 @@
-# get current version from package.json
-
 cd browser/country
 RES1=`find data -name '*.idx' -size -1k 2>/dev/null`
 if [ -z "$RES1" ]; then
 	if ! git diff --quiet HEAD -- *.idx; then
+		# get current version from package.json
 		VERT=`node -p "var j=require('./package.json');j.version=j.version.split('.').slice(0,-1).join('.')+'.'+require('dayjs')().format('YYYYMMDD');require('fs').writeFileSync('./package.json',JSON.stringify(j,null,2));j.version;"`
 		git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 		git config --local user.name "github-actions[bot]"
 		git commit -a -m "v${VERT} auto update ip database"
 		npm publish
+
+		cd ../country-extra
+		RES2=`find data -name '*.idx' -size -1k 2>/dev/null`
+		if [ -z "$RES2" ]; then
+			VERT=`node -p "var j=require('./package.json');j.version=j.version.split('.').slice(0,-1).join('.')+'.'+require('dayjs')().format('YYYYMMDD');require('fs').writeFileSync('./package.json',JSON.stringify(j,null,2));j.version;"`
+			npm publish
+		fi
 	fi
 fi
 
-cd ../..
-
-cd browser/country-extra
-RES2=`find data -name '*.idx' -size -1k 2>/dev/null`
-if [ -z "$RES2" ]; then
-	if ! git diff --quiet HEAD -- *.idx; then
-		VERT=`node -p "var j=require('./package.json');j.version=j.version.split('.').slice(0,-1).join('.')+'.'+require('dayjs')().format('YYYYMMDD');require('fs').writeFileSync('./package.json',JSON.stringify(j,null,2));j.version;"`
-		git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-		git config --local user.name "github-actions[bot]"
-		git commit -a -m "v${VERT} auto update ip database"
-		npm publish
-	fi
-fi
