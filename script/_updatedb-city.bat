d1962e203537e582f0f34c46dc74694747a3aa76
Bas950 <me@bas950.com>
2024-09-28 16:24:19 +0200

    chore: clean

--- script/_updatedb-city.bat
+++
@@ -1,5 +0,0 @@
-
-cd /d %~dp0
-
-node updatedb.js ila_fields=latitude,longitude,area,country,region1,metro,timezone,city
-node updatedb.js ila_fields=latitude,longitude,area,country,region1,metro,timezone,city ila_small_memory=true
