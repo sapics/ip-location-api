b114e9e71e04582ec8806d3948941dd51537d2fd
Bas950 <me@bas950.com>
2024-09-30 23:20:33 +0200

    wip: database

--- packages/util/src/db.ts
+++ packages/util/src/db.ts
@@ -1,7 +1,9 @@
 import type { IpLocationApiInputSettings, IpLocationApiSettings } from './functions/getSettings.js'
 /* eslint-disable jsdoc/check-param-names */
 import { existsSync } from 'node:fs'
-import { mkdir } from 'node:fs/promises'
+import { cp, mkdir, readdir, rename, rm, writeFile } from 'node:fs/promises'
+import path from 'node:path'
+import { DATABASE_SUFFIX_SHA } from './constants.js'
 import { createDatabase } from './functions/database/createDatabase.js'
 import { downloadAndExtractDatabase } from './functions/database/downloadAndExtractDatabase.js'
 import { getSettings } from './functions/getSettings.js'
@@ -11,18 +13,34 @@ import { getSettings } from './functions/getSettings.js'
  * @param inputSettings - Partial settings to override the default ones.
  * @returns An array of extracted file names or false if no update was needed.
  */
-export async function update(inputSettings?: Partial<IpLocationApiInputSettings>): Promise<false | string[]> {
+export async function update(inputSettings?: Partial<IpLocationApiInputSettings>): Promise<void> {
   const settings = getSettings(inputSettings)
   await ensureDirectoriesExist(settings)
-  const { files, dataType } = await downloadAndExtractDatabase(settings)
+  const { files, sha256 } = await downloadAndExtractDatabase(settings)
   if (!files)
-    return false
+    return
 
   // TODO: Add debug log
-  await createDatabase(files, settings, dataType)
+  await createDatabase(files, settings)
   // TODO: Add debug log
 
-  return files
+  //* Save the sha256 hash of the database
+  await writeFile(path.join(settings.fieldDir, `${settings.series}-${settings.dataType}-CSV${DATABASE_SUFFIX_SHA}`), sha256)
+
+  //* Rename the temporary files to the correct name
+  const tmpFiles = (await readdir(settings.fieldDir)).filter(file => file.endsWith('.tmp'))
+  for (const file of tmpFiles) {
+    await rename(path.join(settings.fieldDir, file), path.join(settings.fieldDir, file.replace('.tmp', '')))
+  }
+
+  if (settings.smallMemory) {
+    //* Copy the temporary files to the correct name
+    await cp(path.join(settings.fieldDir, 'v4-tmp'), path.join(settings.fieldDir, 'v4'), { recursive: true, force: true })
+    await cp(path.join(settings.fieldDir, 'v6-tmp'), path.join(settings.fieldDir, 'v6'), { recursive: true, force: true })
+    //* Remove the temporary files
+    await rm(path.join(settings.fieldDir, 'v4-tmp'), { recursive: true, force: true, maxRetries: 3 })
+    await rm(path.join(settings.fieldDir, 'v6-tmp'), { recursive: true, force: true, maxRetries: 3 })
+  }
 }
 
 /**
